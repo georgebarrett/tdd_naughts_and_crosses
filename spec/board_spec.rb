8ce3c5add5f8323cbb7e7f6ee9210c32c1ad2e9b
George Barrett <georgeaustinbarrett@gmail.com>
2023-08-15 12:52:15 +0100

    test added for a scenario where the board is full of 'X' and 'O's. test running GREEN

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -32,4 +32,22 @@ describe Board do
     expect(result[2]).to eq ['X', nil, nil]
   end
 
+  it 'updates the entire board with symbols' do
+    board = Board.new
+    board.update_state(1, 1, 'X')
+    board.update_state(2, 1, 'X')
+    board.update_state(3, 1, 'X')
+    board.update_state(1, 2, 'X')
+    board.update_state(2, 2, 'O')
+    board.update_state(3, 2, 'X')
+    board.update_state(1, 3, 'O')
+    board.update_state(2, 3, 'O')
+    board.update_state(3, 3, 'O')
+    result = board.state
+
+    expect(result[0]).to eq ['O', 'O', 'O']
+    expect(result[1]).to eq ['X', 'O', 'X']
+    expect(result[2]).to eq ['X', 'X', 'X']
+  end
+
 end
