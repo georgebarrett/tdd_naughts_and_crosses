5d9cdcb147bdf2ce9e71462ff5ca2b624aa2ed71
George Barrett <georgeaustinbarrett@gmail.com>
2023-08-15 16:10:54 +0100

    test written for ensuring that the player after player_2, reverts back to player_1. test running GREEN

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -47,6 +47,18 @@ describe Game do
   it 'switches player from player_1 to player_2' do
     player_1 = double('player_1', name: 'George', symbol: 'X')
     player_2 = double('player_2', name: 'Rona', symbol: 'O')
+    board = double('board', is_empty: true, update_state: nil)
+
+    game = Game.new(player_1, player_2, board)
+    game.switch_player
+    result = game.current_player
+
+    expect(result).to eq player_2
+  end
+
+  it 'switches player from player_2 to player_1' do
+    player_1 = double('player_1', name: 'George', symbol: 'X')
+    player_2 = double('player_2', name: 'Rona', symbol: 'O')
     board = double('board', is_empty: false, update_state: nil)
 
     game = Game.new(player_1, player_2, board)
