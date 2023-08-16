5df1dce555dfbc1c007b3d3324f3de8b09a9d8f7
George Barrett <georgeaustinbarrett@gmail.com>
2023-08-16 16:44:17 +0100

    test written for a scenario where all the top horizontal squares are filled with Xs. game status 'complete'. test running GREEN

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -123,4 +123,15 @@ describe Game do
     result = game.status
     expect(result).to eq 'complete'
   end
+
+  it 'returns complete when top horizontal line is claimed by X' do
+    player_1 = double('player_1', name: 'Player 1 Name', symbol: 'X')
+    player_2 = double('player_2', name: 'Player 2 Name', symbol: 'O')
+    board = double('board', is_empty: false, update_state: nil, state: [['X', 'X', 'X'], [nil, nil, nil], [nil, nil, nil]])
+
+    game = Game.new(player_1, player_2, board)
+
+    result = game.status
+    expect(result).to eq 'complete'
+  end
 end
