75b5cabcbd63be4503952885e1bf7e8bfe07afb4
George Barrett <georgeaustinbarrett@gmail.com>
2023-08-16 16:46:24 +0100

    test written for a scenario where the top horizontal line is filled with Os. game status 'complete'. test running GREEN

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -134,4 +134,15 @@ describe Game do
     result = game.status
     expect(result).to eq 'complete'
   end
+
+  it 'returns complete when top horizontal line is claimed by O' do
+    player_1 = double('player_1', name: 'Player 1 Name', symbol: 'X')
+    player_2 = double('player_2', name: 'Player 2 Name', symbol: 'O')
+    board = double('board', is_empty: false, update_state: nil, state: [['O', 'O', 'O'], [nil, nil, nil], [nil, nil, nil]])
+
+    game = Game.new(player_1, player_2, board)
+
+    result = game.status
+    expect(result).to eq 'complete'
+  end
 end
