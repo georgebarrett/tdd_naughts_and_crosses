9b897dc062e7fc57a4647293a16b37d2a0718990
George Barrett <georgeaustinbarrett@gmail.com>
2023-08-16 16:50:29 +0100

    test written for a scenario where the first vertical squares are filled with Xs. game status 'complete'. test running GREEN

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -167,4 +167,15 @@ describe Game do
     result = game.status
     expect(result).to eq 'complete'
   end
+
+  it 'returns complete when first vertical line is claimed by X' do
+    player_1 = double('player_1', name: 'Player 1 Name', symbol: 'X')
+    player_2 = double('player_2', name: 'Player 2 Name', symbol: 'O')
+    board = double('board', is_empty: false, update_state: nil, state: [['X', nil, nil], ['X', nil, nil], ['X', nil, nil]])
+
+    game = Game.new(player_1, player_2, board)
+
+    result = game.status
+    expect(result).to eq 'complete'
+  end
 end
